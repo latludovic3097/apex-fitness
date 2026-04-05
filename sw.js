34756c2dd68e4c2b029bd0f05da4d68ce6b7675b
latludovic3097 <lat.ludovic@gmail.com>
2026-04-05 16:19:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='apex-v5';
+const CACHE='apex-v6';
 const URLS=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS))));
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.status===200){const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));}return res;}).catch(()=>caches.match('./index.html'))));});
